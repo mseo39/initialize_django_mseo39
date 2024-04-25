eb3f053c094d56ba47fe85761c2ef17c8d449cd7
mseo39 <a01063294905@gmail.com>
2024-04-25 21:17:25 +0900

    add User model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.CharField(max_length=30)
+    age = models.IntegerField
+    
+    class meta:
+        db_table = 'user'
