7ba66a010bdf598fd353e7d32be036d0d5858aca
mseo39 <a01063294905@gmail.com>
2024-04-26 04:00:10 +0900

    add refresh로 access 재발급

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 import user.views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('session_signup', user.views.session_signup, name='session_signup'),
     path('jwt_signin', user.views.jwt_signin, name='jwt_signin'),
     path('only_authenticated_user', user.views.only_authenticated_user, name='only_authenticated_user'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
